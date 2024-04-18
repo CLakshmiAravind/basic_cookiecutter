2d63b73ce3c2bec8523f4e48770a522dcf3a2d63
CLakshmiAravind <n170848@rguktn.ac.in>
2024-04-18 18:51:00 +0530

    modified post_gen_script

--- post_gen_script.py
+++ post_gen_script.py
@@ -1,14 +1,21 @@
-# post_gen_script.py
+# post_gen_project.py
 
 import os
 import shutil
 
-def run_post_gen_hook(install_path, project_name, file_name):
+def run_post_gen_hook(install_path, folder_name, file_name):
     # Create directory if it doesn't exist
     if not os.path.exists(install_path):
         os.makedirs(install_path)
     
     # Copy files to the installation path
     # Modify this part according to your setup requirements
-    shutil.copytree('path/to/setup_files', os.path.join(install_path, project_name))
+    shutil.copytree('path/to/setup_files', os.path.join(install_path, folder_name))
     print(f"Setup completed in {install_path}")
+
+# Run the post generation hook
+run_post_gen_hook(
+    '{{ cookiecutter.install_path }}',
+    '{{ cookiecutter.project_name }}',
+    '{{ cookiecutter.file_name }}'
+)
