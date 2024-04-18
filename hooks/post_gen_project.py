256c19013c3ce0c7b94a5658772749019b596ad9
CLakshmiAravind <n170848@rguktn.ac.in>
2024-04-18 19:59:04 +0530

    modified post_gen_script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,11 +12,11 @@ def run_post_gen_hook(install_path="{{cookiecutter.install_path}}", folder_name=
     
     # Copy files to the installation path
     # Modify this part according to your setup requirements
+    cwd = os.getcwd()
     if(install_path=='./'):
-        pass
+        shutil.copytree('./', cwd)
     else:
         shutil.copytree('./', os.path.join(install_path, folder_name))
-        cwd = os.getcwd()
         shutil.rmtree(cwd)
     # print(os.listdir())
     print(f"Setup completed in {install_path}{folder_name}")
